eb058381dafdc0072dcc4d11e8d9e35872530150
huangancheng <huangancheng@bytedance.com>
2020-12-14 18:40:47 +0800

    feat: add website

---
+++ website/src/stories/Package.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import Button, { ButtonProps } from '@huangancheng/core';
+
+export default {
+  title: 'Example/Package',
+  component: Button,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as Meta;
+
+const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  children: '@huangancheng/core',
+};
