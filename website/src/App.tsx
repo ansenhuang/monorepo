700e0b7993b82a7e458f44d9ac270bbcad4da5e6
ansenhuang <ancheng1992@126.com>
2020-12-18 23:03:05 +0800

    feat: add custom router

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,34 +1,28 @@
-import React, { lazy, Suspense, useState, useEffect } from 'react';
-import { RouterContext } from './router';
+import React, { lazy, Suspense } from 'react';
+import { Router, Route } from './router';
 
-interface Pages {
-  [pathname: string]: React.LazyExoticComponent<() => JSX.Element>;
-}
-const pages: Pages = {
-  '/': lazy(() => import('./pages/app')),
-  '/package': lazy(() => import('./pages/package')),
-};
+const pages = [
+  {
+    path: '/',
+    Component: lazy(() => import('./pages/app')),
+  },
+  {
+    path: '/package',
+    Component: lazy(() => import('./pages/package')),
+  },
+];
 
 const App = () => {
-  const [pathname, setPathname] = useState(window.location.pathname);
-  const Page = pages[pathname];
-
-  useEffect(() => {
-    const onHistoryChange = (e: PopStateEvent) => {
-      setPathname(window.location.pathname);
-    };
-    window.addEventListener('popstate', onHistoryChange);
-    return () => {
-      window.removeEventListener('popstate', onHistoryChange);
-    };
-  }, []);
-
   return (
-    <RouterContext.Provider value={{ pathname, setPathname }}>
-      <Suspense fallback={null}>
-        {Page ? <Page /> : <h1 style={{ textAlign: 'center' }}>404</h1>}
-      </Suspense>
-    </RouterContext.Provider>
+    <Router>
+      {pages.map(({ path, Component, ...restProps }) => (
+        <Route key={path} path={path} {...restProps}>
+          <Suspense fallback={null}>
+            <Component />
+          </Suspense>
+        </Route>
+      ))}
+    </Router>
   );
 };
 
--- website/src/router.tsx
+++ website/src/router.tsx
@@ -1,24 +1,55 @@
-import React, { createContext, useContext } from 'react';
+import React, { createContext, useContext, useState, useEffect } from 'react';
+
+const getCurrentPath = () => window.location.pathname;
+const initialPath = getCurrentPath();
 
 interface RouterContextValue {
-  pathname: string;
-  setPathname: (pathname: string) => void;
+  path: string;
+  setPath: (url: string) => void;
 }
-
-export const RouterContext = createContext<RouterContextValue>({
-  pathname: '',
-  setPathname: () => {},
+const RouterContext = createContext<RouterContextValue>({
+  path: initialPath,
+  setPath: () => {},
 });
 
-export const useRouter = (): [string, (url: string) => void] => {
-  const { pathname, setPathname } = useContext(RouterContext);
+export const useRouter = (): [string, (path: string) => void] => {
+  const { path, setPath } = useContext(RouterContext);
   return [
-    pathname,
-    (url: string) => {
-      window.history.pushState(null, '', url);
-      setPathname(url);
+    path,
+    (nextPath: string) => {
+      window.history.pushState(null, '', nextPath);
+      setPath(nextPath);
     },
   ];
+};
+
+interface RouterProps {}
+export const Router: React.FC<RouterProps> = ({ children }) => {
+  const [path, setPath] = useState(initialPath);
+
+  useEffect(() => {
+    const onHistoryChange = (e: PopStateEvent) => {
+      setPath(getCurrentPath());
+    };
+    window.addEventListener('popstate', onHistoryChange);
+    return () => {
+      window.removeEventListener('popstate', onHistoryChange);
+    };
+  }, []);
+
+  return <RouterContext.Provider value={{ path, setPath }}>{children}</RouterContext.Provider>;
+};
+
+interface RouteProps {
+  path: string;
+  children?: React.ReactElement<any, any> | null;
+}
+export const Route: React.FC<RouteProps> = ({ path, children }) => {
+  const { path: contextPath } = useContext(RouterContext);
+  if (path !== contextPath) {
+    return null;
+  }
+  return children || null;
 };
 
 interface LinkProps
@@ -29,12 +60,12 @@ interface LinkProps
   to: string;
 }
 export const Link: React.FC<LinkProps> = ({ to, style, onClick, children, ...restProps }) => {
-  const [, setPathname] = useRouter();
+  const [, setPath] = useRouter();
 
   const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
     e.preventDefault();
     onClick?.(e);
-    setPathname(to);
+    setPath(to);
   };
 
   return (
